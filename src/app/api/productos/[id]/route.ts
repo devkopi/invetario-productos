61449bc12bcf2852b3617a6fa5da4cfc9b679058
devkopis <manugualdron@gmail.com>
2025-06-01 10:27:17 -0400

    Actualizar lógica y errores en rutas PUT y DELETE

--- src/app/api/productos/[id]/route.ts
+++ src/app/api/productos/[id]/route.ts
@@ -1,28 +1,42 @@
 import { NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
-export async function PUT(request: NextRequest, context: { params: { id: string } }) {
-  const id = parseInt(context.params.id);
-  const body = await request.json();
+export async function PUT(req: NextRequest, context: { params: { id: string } }) {
+    try {
+        const id = parseInt(context.params.id);
+        const body = await req.json();
 
-  const productoActualizado = await prisma.producto.update({
-    where: { id },
-    data: body,
-  });
+        const productoActualizado = await prisma.producto.update({
+            where: { id },
+            data: body,
+        });
 
-  return new Response(JSON.stringify(productoActualizado), {
-    headers: { 'Content-Type': 'application/json' },
-  });
+        return Response.json(productoActualizado);
+    } catch (error) {
+        console.error("Error al actualizar producto: ", error)
+        return new Response(JSON.stringify({ error: 'Error al actualizar producto' }), {
+            status: 500,
+        });
+    }
 }
 
 
 export async function DELETE(request: NextRequest, context: { params: { id: string } }) {
-  const id = parseInt(context.params.id);
+  try {
+    const id = parseInt(context.params.id);
 
-  await prisma.producto.delete({
-    where: { id },
-  });
+    await prisma.producto.delete({
+      where: { id },
+    });
 
-  return new Response(null, { status: 204 });
+    return new Response(null, { status: 204 });
+  } catch (error) {
+    console.error('Error al eliminar producto:', error);
+
+    return new Response(
+      JSON.stringify({ error: 'Error al eliminar producto' }),
+      { status: 500 }
+    );
+  }
 }
 
