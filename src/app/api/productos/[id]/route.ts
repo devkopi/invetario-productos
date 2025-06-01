0628fe6ccc7339cd2976adea1cc83533e073a195
devkopis <manugualdron@gmail.com>
2025-06-01 10:44:26 -0400

    Corregido tipado de context en métodos PUT y DELETE

--- src/app/api/productos/[id]/route.ts
+++ src/app/api/productos/[id]/route.ts
@@ -1,46 +1,47 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function PUT(
-  req: NextRequest,
-  { params }: { params: { id: string } }
+    request: NextRequest,
+    context: { params: { id: string } }
 ) {
-  try {
-    const id = parseInt(params.id);
-    const body = await req.json();
+    const id = parseInt(context.params.id);
+    const body = await request.json();
 
-    const productoActualizado = await prisma.producto.update({
-      where: { id },
-      data: body,
-    });
+    try {
+        const productoActualizado = await prisma.producto.update({
+            where: { id },
+            data: body,
+        });
 
-    return NextResponse.json(productoActualizado);
-  } catch (error) {
-    console.error("Error al actualizar producto: ", error);
-    return NextResponse.json(
-      { error: 'Error al actualizar producto' },
-      { status: 500 }
-    );
-  }
+        return Response.json(productoActualizado);
+    } catch (error) {
+        console.error("Error al actualizar el producto: ", error)
+        return new Response(
+            JSON.stringify({ error: 'Error al actualizar producto' }),
+            { status: 500 }
+        );
+    }
 }
 
+
 export async function DELETE(
-  req: NextRequest,
-  { params }: { params: { id: string } }
+    request: NextRequest,
+    context: { params: { id: string } }
 ) {
-  try {
-    const id = parseInt(params.id);
+    const id = parseInt(context.params.id);
 
-    await prisma.producto.delete({
-      where: { id },
-    });
+    try {
+        await prisma.producto.delete({
+            where: { id },
+        });
 
-    return new NextResponse(null, { status: 204 });
-  } catch (error) {
-    console.error('Error al eliminar producto:', error);
-    return NextResponse.json(
-      { error: 'Error al eliminar producto' },
-      { status: 500 }
-    );
-  }
+        return new Response(null, { status: 204 });
+    } catch (error) {
+        console.error("Error en eliminar el producto: ", error)
+        return new Response(
+            JSON.stringify({ error: 'Error al eliminar producto' }),
+            { status: 500 }
+        );
+    }
 }
